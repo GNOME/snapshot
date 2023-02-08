ce057f22cc5f92a1d88b804f53528a22dc165169
Maximiliano Sandoval R <msandova@gnome.org>
2023-02-08 14:54:47 +0100

    shutter_button: Port to gtk::Snapshot

--- src/widgets/shutter_button.rs
+++ src/widgets/shutter_button.rs
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-use std::f64::consts::PI;
-
 use adw::prelude::*;
 use gettextrs::gettext;
 use gtk::subclass::prelude::*;
-use gtk::{cairo, glib, graphene};
+use gtk::{gdk, glib, graphene, gsk};
 
 use crate::ShutterMode;
+
+/// Magic factor: Consider a square with width = w, then inscribe a circle
+/// inside it and then a square inside the circle. LAMBDA * w represents the
+/// distance between the small and the big square, which is precisely the
+/// translation we need for the record animation. Equals to (sqrt(2) - 1) / 2.
+///
+/// ```
+/// assert!((LAMBDA - (2.0_f32.sqrt() / 2.0 - 0.5)).abs() < f32::EPSILON);
+/// ```
+const LAMBDA: f32 = 0.20710677;
 
 mod imp {
     use super::*;
@@ -20,6 +28,8 @@ mod imp {
 
         pub countdown: Cell<u32>,
 
+        // TODO Remove this, we can query the value directly from the
+        // animations.
         pub countdown_val: Cell<f64>,
         pub record_val: Cell<f64>,
         pub press_val: Cell<f64>,
@@ -109,7 +119,7 @@ mod imp {
                     widget.imp().press_val.set(value);
                     widget.queue_draw();
                 }));
-            let press_ani = adw::TimedAnimation::new(&*widget, 0.0, 3.0, 125, &press_target);
+            let press_ani = adw::TimedAnimation::new(&*widget, 3.0, 6.0, 125, &press_target);
             self.press_ani.set(press_ani).unwrap();
 
             let mode_target =
@@ -162,60 +172,14 @@ mod imp {
         fn snapshot(&self, snapshot: &gtk::Snapshot) {
             let widget = self.obj();
 
-            let width = widget.width() as f64;
-            let height = widget.height() as f64;
-            let size = width.min(height);
+            let width = widget.width();
+            let height = widget.height();
 
-            let line = (size / 8.0).min(8.0);
+            let size = width.min(height) as f32;
+            let border_width = (size / 8.0).min(8.0);
 
-            let color = if widget.is_sensitive() { 1.0 } else { 0.5 };
-            let mode_color = self.mode_val.get() * color;
-            let alpha = widget.color().alpha() as f64;
-
-            let rect = graphene::Rect::new(0.0, 0.0, width as f32, height as f32);
-            let ctx = snapshot.append_cairo(&rect);
-
-            ctx.set_line_width(line);
-            ctx.set_source_rgba(color, color, color, alpha);
-            ctx.arc_negative(
-                width / 2.0,
-                height / 2.0,
-                (size - line) / 2.0,
-                1.5 * PI,
-                (2.0 * self.countdown_val.get() - 1.5) * -PI,
-            );
-            ctx.stroke().unwrap();
-
-            ctx.set_source_rgba(color, mode_color, mode_color, alpha);
-
-            let record = self.record_val.get();
-            let gap = if matches!(self.shutter_mode.get(), crate::ShutterMode::Picture) {
-                self.press_val.get()
-            } else {
-                3.0
-            };
-
-            if record == 0.0 {
-                ctx.arc(
-                    width / 2.0,
-                    height / 2.0,
-                    size / 2.0 - line - gap,
-                    0.0,
-                    2.0 * PI,
-                );
-            } else {
-                let gap = gap + 3.0 * record;
-                let sq_radius = ((1.0 - record) * size / 2.0 - line - gap).max(3.0);
-                let sq_size = (((size / 2.0 - line - gap) - sq_radius
-                    + (2.0 * sq_radius.powi(2)).sqrt())
-                .powi(2)
-                    / 2.0)
-                    .sqrt();
-
-                rounded_square(&ctx, sq_size, sq_radius, width / 2.0, height / 2.0);
-            }
-
-            ctx.fill().unwrap();
+            widget.draw_border(snapshot, size, border_width);
+            widget.draw_play(snapshot, size, border_width);
         }
     }
 
@@ -298,7 +262,7 @@ impl ShutterButton {
                     self.mode_ani().play();
 
                     self.record_ani().set_value_from(record_from);
-                    self.record_ani().set_value_to(1.0);
+                    self.record_ani().set_value_to(0.7);
                     self.record_ani().play();
                 }
             }
@@ -310,21 +274,85 @@ impl ShutterButton {
     pub fn shutter_mode(&self) -> ShutterMode {
         self.imp().shutter_mode.get()
     }
-}
 
-fn rounded_square(ctx: &cairo::Context, size: f64, radius: f64, x: f64, y: f64) {
-    let x = x - size;
-    let y = y - size;
-    let size = size * 2.0;
+    fn draw_border(&self, snapshot: &gtk::Snapshot, size: f32, border_width: f32) {
+        // Magic matrix that turns the inner blue circle transparent.
+        #[rustfmt::skip]
+        let color_matrix = graphene::Matrix::from_float([
+            1.0, -1.0, -1.0,  0.0,
+            1.0,  1.0,  1.0,  1.0,
+            1.0,  1.0,  1.0, -1.0,
+            0.0,  0.0,  0.0,  1.0,
+        ]);
+        let color_offset = graphene::Vec4::from_float([0.0; 4]);
+        snapshot.push_color_matrix(&color_matrix, &color_offset);
 
-    // top right
-    ctx.arc(x + size - radius, y + radius, radius, -0.5 * PI, 0.0);
-    // bottom right
-    ctx.arc(x + size - radius, y + size - radius, radius, 0.0, 0.5 * PI);
-    // bottom left
-    ctx.arc(x + radius, y + size - radius, radius, 0.5 * PI, PI);
-    // top left
-    ctx.arc(x + radius, y + radius, radius, PI, -0.5 * PI);
-    // back to top right
-    ctx.line_to(x + size - radius, y);
+        let countdown = self.imp().countdown_val.get() as f32;
+
+        let rect = graphene::Rect::new(0.0, 0.0, size, size);
+        let center = graphene::Point::new(size / 2.0, size / 2.0);
+
+        let s = graphene::Size::new(size / 2.0, size / 2.0);
+        let rounded = gsk::RoundedRect::new(rect, s, s, s, s);
+
+        let color = self.color();
+
+        let stop0 = gsk::ColorStop::new(countdown, color);
+        let stop1 = gsk::ColorStop::new(countdown, gdk::RGBA::new(0.0, 0.0, 0.0, 0.0));
+
+        snapshot.push_rounded_clip(&rounded);
+        snapshot.append_conic_gradient(&rect, &center, 0.0, &[stop0, stop1]);
+        snapshot.pop();
+
+        // We draw a blue circle, later the color matrix will turn blue into
+        // transparent goodness.
+        let inner_size = size - 2.0 * border_width;
+        let inner_rect = graphene::Rect::new(border_width, border_width, inner_size, inner_size);
+        let inner_s = graphene::Size::new(inner_size / 2.0, inner_size / 2.0);
+        let inner_rounded = gsk::RoundedRect::new(inner_rect, inner_s, inner_s, inner_s, inner_s);
+
+        snapshot.push_rounded_clip(&inner_rounded);
+        // We color on a bigger rect than what we clipped.
+        snapshot.append_color(&gdk::RGBA::BLUE, &rect);
+        snapshot.pop();
+
+        snapshot.pop(); // Pop the color matrix.
+    }
+
+    fn draw_play(&self, snapshot: &gtk::Snapshot, size: f32, border_width: f32) {
+        let imp = self.imp();
+
+        // When we want to transform the button to a square, we don't use the
+        // gap animation, otherwise it looks janky as its size gets smaller due
+        // to the record animation and its size at first is momentarily reduced
+        // due to the press animation, but later it gets bigger.
+        let gap = if matches!(imp.shutter_mode.get(), crate::ShutterMode::Picture)
+            || self.countdown() > 0
+        {
+            imp.press_val.get() as f32
+        } else {
+            3.0
+        };
+        let record = imp.record_val.get() as f32;
+
+        let initial_r_size = size - 2.0 * gap - 2.0 * border_width;
+        let x = gap + border_width + initial_r_size * LAMBDA * record;
+        let y = x;
+        let rect_size = size - 2.0 * x;
+        let rect = graphene::Rect::new(x, y, rect_size, rect_size);
+        let big_rect = graphene::Rect::new(0.0, 0.0, size, size);
+
+        let border_radius = (1.0 - record) * rect_size / 2.0;
+        let s = graphene::Size::new(border_radius, border_radius);
+        let rounded = gsk::RoundedRect::new(rect, s, s, s, s);
+
+        let color = self.color().red();
+        let alpha = self.color().alpha();
+        let mode_color = imp.mode_val.get() as f32 * color;
+        let mode_color = gdk::RGBA::new(color, mode_color, mode_color, alpha);
+        snapshot.push_rounded_clip(&rounded);
+        // We color on a bigger rect than what we clipped.
+        snapshot.append_color(&mode_color, &big_rect);
+        snapshot.pop();
+    }
 }
