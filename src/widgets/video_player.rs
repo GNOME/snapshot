79eb77632e7c37937ee953cdf116e213e40b7087
Jamie Murphy <hello@itsjamie.dev>
2024-07-01 12:01:11 -0700

    Reapply "video_player: Offload DMABUFs"
    This reverts commit d0ed08747cece2375e3ad36002803347b900b120.

--- src/widgets/video_player.rs
+++ src/widgets/video_player.rs
@@ -10,6 +10,7 @@ mod imp {
 
     #[derive(Debug, Default)]
     pub struct VideoPlayer {
+        pub offload: gtk::GraphicsOffload,
         pub media_file: gtk::MediaFile,
         pub picture: gtk::Picture,
         pub controls: gtk::MediaControls,
@@ -39,8 +40,9 @@ mod imp {
             self.controls.set_halign(gtk::Align::Fill);
             self.controls.set_hexpand(true);
             self.controls.add_css_class("videoplayercontrols");
+            self.offload.set_child(Some(&self.picture));
 
-            widget.set_child(Some(&self.picture));
+            widget.set_child(Some(&self.offload));
 
             let id = self.media_file.connect_invalidate_contents(glib::clone!(
                 #[weak]
