c33a6dbbd6ceb41084b0353fa094970b8ce8feac
Jamie Murphy <hello@itsjamie.dev>
2024-09-15 11:30:49 -0700

    cargo: Vendor with all features
    Without this, crates that are only used by a feature (such as x11)
    aren't included in the vendored tarball.
    
    Fixes: #229

--- build-aux/dist-vendor.sh
+++ build-aux/dist-vendor.sh
@@ -9,7 +9,7 @@ cd "$SOURCE_ROOT"
 mkdir "$DIST"/.cargo
 # cargo-vendor-filterer can be found at https://github.com/coreos/cargo-vendor-filterer
 # It is also part of the Rust SDK extension.
-cargo vendor-filterer --platform=x86_64-unknown-linux-gnu --platform=aarch64-unknown-linux-gnu > "$DIST"/.cargo/config
+cargo vendor-filterer --all-features --platform=x86_64-unknown-linux-gnu --platform=aarch64-unknown-linux-gnu > "$DIST"/.cargo/config
 rm -f vendor/gettext-sys/gettext-*.tar.*
 # remove the tarball from checksums
 echo $(jq -c 'del(.files["gettext-0.21.tar.xz"])' vendor/gettext-sys/.cargo-checksum.json) > vendor/gettext-sys/.cargo-checksum.json
