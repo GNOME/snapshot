GNOME/snapshot

commit e60bdb67e7d9bad8016741ba40d6a560e2478653

aperture: Do not assume sources are pipewiresrc

aperture/src/camera.rs
@@ -145,7 +145,7 @@ impl Camera {
     }
 
     pub(crate) fn create_element(&self) -> Result<gst::Element, glib::BoolError> {
-        let element = self.device().create_element(None)?;
+        let element = self.device().create_element(Some("pipewiresrc"))?;
         element.set_property("client-name", crate::APP_ID.get().unwrap());
         Ok(element)
     }

commit 2cf4643b0abc1a238f8f38c3eb8250a347ccb10d

aperture: Only get devices from the pipewiredeviceprovider
`create_element`'s parameter is for giving the produced element a name.
It does not select the element type.

Remove the `DeviceMonitor` as it can load and start other providers that
might give us incompatible cameras while the pipewire provider is still
busy figuring out whether it should hide these other providers.

Using a device monitor inside a device provider (which itself implicitly
feeds into the monitor) also introduces recursion, which is probably
unwise.

Do the filtering the monitor did for us ourselves.

Fixes: e60bdb67e7d9bad8016741ba40d6a560e2478653
Fixes: https://gitlab.gnome.org/GNOME/snapshot/-/issues/232

aperture/src/camera.rs
@@ -145,7 +145,7 @@ impl Camera {
     }
 
     pub(crate) fn create_element(&self) -> Result<gst::Element, glib::BoolError> {
-        let element = self.device().create_element(Some("pipewiresrc"))?;
+        let element = self.device().create_element(None)?;
         element.set_property("client-name", crate::APP_ID.get().unwrap());
         Ok(element)
     }

aperture/src/device_provider.rs
@@ -28,7 +28,6 @@ mod imp {
     #[properties(wrapper_type = super::DeviceProvider)]
     pub struct DeviceProvider {
         pub inner: OnceCell<gst::DeviceProvider>,
-        pub monitor: OnceCell<gst::DeviceMonitor>,
         pub cameras: RefCell<Vec<crate::Camera>>,
         pub bus_watch: OnceCell<gst::bus::BusWatchGuard>,
 
@@ -107,10 +106,6 @@ mod imp {
             self.parent_constructed();
 
             crate::ensure_init();
-
-            let monitor = gst::DeviceMonitor::new();
-            monitor.add_filter(Some("Video/Source"), Some(&crate::SUPPORTED_CAPS));
-            self.monitor.set(monitor).unwrap();
 
             if let Some(provider) = gst::DeviceProviderFactory::by_name("pipewiredeviceprovider") {
                 self.inner.set(provider).unwrap();
@@ -215,12 +210,11 @@ impl DeviceProvider {
         };
         provider.start()?;
 
-        let monitor = imp.monitor.get().unwrap();
-
         let mut seen = HashSet::new();
-        let mut cameras = monitor
+        let mut cameras = provider
             .devices()
             .iter()
+            .filter(|d| is_camera(d))
             .map(crate::Camera::new)
             .filter(|d| !is_ir_camera(d))
             .collect::<Vec<_>>();
@@ -238,7 +232,7 @@ impl DeviceProvider {
         self.imp().cameras.replace(cameras);
         self.items_changed(0, 0, n_items);
 
-        let bus = monitor.bus();
+        let bus = provider.bus();
         let watch = bus
             .add_watch_local(glib::clone!(
                 #[weak(rename_to = obj)]
@@ -359,6 +353,9 @@ impl DeviceProvider {
                     .structure()
                     .and_then(|s| s.get::<gst::Device>("device").ok())
                 {
+                    if !is_camera(&device) {
+                        return;
+                    }
                     let device = crate::Camera::new(&device);
                     if !imp.has_camera(&device) {
                         // We ignore/filter IR cameras.
@@ -403,6 +400,13 @@ impl DeviceProvider {
             _ => (),
         }
     }
+}
+
+fn is_camera(device: &gst::Device) -> bool {
+    device.has_classes("Video/Source")
+        && device
+            .caps()
+            .is_some_and(|c| c.can_intersect(&crate::SUPPORTED_CAPS))
 }
 
 fn is_ir_camera(device: &crate::Camera) -> bool {